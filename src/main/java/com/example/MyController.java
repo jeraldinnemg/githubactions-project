3c5fbc065e1a2853d6b5eebf0438de451bd5edc3
Jeraldinne Molleda <jeraldinnemg@gmail.com>
2025-02-06 14:48:39 -0300

    adding-version-app

--- src/main/java/com/example/MyController.java
+++ src/main/java/com/example/MyController.java
@@ -5,16 +5,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/v2/api")
 public class MyController {
 
     @GetMapping("/status")
     public String getStatus() {
-        return "App is running!";
+        return "App v2 is running!";
     }
 
     @GetMapping("/")
     public String home() {
-        return "Welcome to the Java App!";
+        return "Welcome to the Java App v2!";
     }
 }
